dd6b89b8abad848a07658813a9419620908a45f2
rick xuoxod <xuoxod@gmail.com>
2023-12-09 06:39:33 -0500

    Updated jwt functions

--- internal/utils/jwtgenerator.go
+++ internal/utils/jwtgenerator.go
@@ -23,3 +23,20 @@ func GenerateJwt(user interface{}) (string, error) {
 
 	return token, nil
 }
+
+func GenerateJwtAfter(user interface{}, timeFrame int) (string, error) {
+	var tf time.Duration = time.Duration(timeFrame)
+
+	claims := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+		Issuer:    fmt.Sprintf("%d", user),
+		ExpiresAt: time.Now().Add(time.Hour * tf).Unix(),
+	})
+
+	token, err := claims.SignedString([]byte(constants.SecretKey))
+
+	if err != nil {
+		return "", errors.New("Unabled to generate token")
+	}
+
+	return token, nil
+}
